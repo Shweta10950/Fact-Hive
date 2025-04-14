140ea83f36629326db6edebf4a4f2d2e297016f4
Shweta10950 <140787657+Shweta10950@users.noreply.github.com>
2025-04-14 20:09:02 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,13 +2,17 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import re
+import re 
+import nltk
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+
+
 
 
 #load data
